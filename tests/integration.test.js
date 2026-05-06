0b438780091fd6f06c7900339d894693b5976841
troy <tagoody15@yahoo.com>
2026-05-06 13:29:12 -0700

    Added unit tests and utils.js for testing purposes

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,5 +1,10 @@
 import { describe, it, beforeEach } from "node:test";
-supertest
+import supertest from "supertest";
+import assert from "node:assert";
+import app from "../app.js";
+
+const request = supertest(app);
+
 describe("Integration Tests", () => {  //describe means a high level test function, inside a describe block can add a ton of tests
   it("performs integration tests correctly", () => {
     // Test implementation
@@ -12,4 +17,3 @@ import assert from //not done
 
 
 it()  //smaller level test function
-
